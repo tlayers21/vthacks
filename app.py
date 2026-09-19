ab8e50018968fed2df50c000751bfe8b98f5f179
bschemms <bschemms@gmail.com>
2026-09-19 11:48:16 -0400

    fixed mistake in app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 # Example of connecting to database
-conn = sqlite3.connect("app.db")
+conn = sqlite3.connect("db/app.db")
 cursor = conn.cursor()
-cursor.execute("SELECT name FROM users;")
+cursor.execute("SELECT name FROM customers;")
 results = cursor.fetchall()
 print(results)
