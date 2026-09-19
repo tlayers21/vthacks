5ab9317c0ff0af3d57b4edb7b9e7c8e548d3d305
bschemms <bschemms@gmail.com>
2026-09-19 13:54:16 -0400

    updated app and added finance page

--- src/app.py
+++ src/app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
-import sqlite3
-from db.init_db import DB_PATH
+from db.connect import transaction, connect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/finance")
+def finance():
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT c.name AS customer_name, c.role, d.name AS department_name "
+        "FROM customers c JOIN departments d ON c.department_id = d.department_id;"
+    )
+    customers = cursor.fetchall()
+    return render_template("finance.html", customers=customers)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
